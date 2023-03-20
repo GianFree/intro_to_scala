c5f3e7f96b9d1d3a23f873f26041f651b6947350
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2023-03-20 21:18:23 +0100

    second episode

--- src/main/scala/com.gianfree/Basics.scala
+++ src/main/scala/com.gianfree/Basics.scala
@@ -10,4 +10,27 @@ object Basics  extends App {
   val aString = "ciao"
   val aComposedString: String = "i" + " " + "love"
   val interpolatedString = s"lol is $meaningOfLife" 
+
+  // code blocks
+  val aCodeBlock = {
+    val aLocalValue = 67
+
+    // need to return something
+    aLocalValue + 3
+  }
+
+
+  def myFunction(x: Int, y: String): String = {
+    y + " " + x
+  }
+
+  def factorial(n: Int): Int =
+    if (n<= 1) 1
+    else n*factorial(n-1)
+
+  
+  // In scala we don't use loop or interation, they use RECURSION
+
+  
+
 }
