7931c9a220da7aa05b3130a2d3cbd47cd0890eb5
Gianfranco Abrusci <gabrusci@liveintent.com>
2023-04-13 11:55:52 +0200

    lol

--- src/main/scala/com.gianfree/FunctionalProgramming.scala
+++ src/main/scala/com.gianfree/FunctionalProgramming.scala
@@ -1,5 +1,24 @@
 package `com.gianfree`
 
 object FunctionalProgramming  extends App {
-  
+  class Person(name: String) {
+    def apply(age: Int) = println(s"I have $age years.")
+  }
+
+  val bob = new Person("Bob")
+  bob(43)
+  // The method apply allows us to use  bob as a function!!!!
+
+
+  // All scala functions are instances of these function_X types.
+  // Function1[T,T] takes 1 arg of typoe T and return a type T
+  // You can have up to Function22
+
+  // Function1[Int,Int] === Int => Int
+
+
+  // higher-order function: function that returns a function
+   
 }
+
+
